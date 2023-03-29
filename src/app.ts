37d47b674a50724c1649dd78098bb66da5f10764
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-03-29 11:17:33 -0300

    ✨ feat: add static files path

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import e from 'express';
 import { routes } from './routes';
+import path from 'node:path';
 
 const app = e();
 
+app.use('/uploads', e.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(e.json());
 app.use(routes);
 
