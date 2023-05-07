f069a1e842ec63c0739ee8a44e545d62661eeee7
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-05-07 12:48:38 -0300

    ✏️ fix: fix typo

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(exception);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Controll-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
 
   next();
 });
