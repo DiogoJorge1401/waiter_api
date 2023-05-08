ca7843cd5f8c165a2ef6a79c340fb38cc30ad366
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-05-07 23:30:49 -0300

    ♻️ refactor: intialize server with http module

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { app } from './app';
 import { PORT, URL } from './configs/vars';
+import http from 'node:http';
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
   console.log(`Server is running on ${URL}`);
 });
