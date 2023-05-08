698adf8dc23da62e5b975fc06a3df30a8339e939
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-05-07 23:45:27 -0300

    ✨ feat: add websocket into app

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import { app } from './app';
 import { PORT, URL } from './configs/vars';
 import http from 'node:http';
+import { Server } from 'socket.io';
 
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: '*',
+  },
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on ${URL}`);
